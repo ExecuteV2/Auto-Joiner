f08d63933a3036686c3b78608825390d743ccbf3
selenne <singkitangnais@gmail.com>
2025-07-31 20:18:58 +0800

    Update ds.lua

--- DarkScripts/ds.lua
+++ DarkScripts/ds.lua
@@ -1,3 +1,3 @@
-Token = "MTA5NjAxMDM5NDM2NTA3MTM4MA.GCDVDH.WEY2DPqFHFBByaiHRmaskhuHFF6_uUL47sFQJ4"
+Token = "MTA5NjAxMDM5NDM2NTA3MTM4MA.G5i79r.kUu-e6wupklEN9NTYJAH1N04IQhezy1weGVEYo"
 Webhook = "https://discord.com/api/webhooks/1397153441846919218/XM0YvDbiD5ew8j6Ccc9qR7KMuISvvg_jhrNJIKelH51UFCT1vzdepGOz-zyK3zPv8W9j"
 loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/AutoJoiner.lua"))()
